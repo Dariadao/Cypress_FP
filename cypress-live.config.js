e756cf9b03d95690aa1cd47313c66573ed87c5d0
Daria <daria361987@gmail.com>
2024-07-17 22:16:32 +0200

    minor changes

--- cypress-live.config.js
+++ cypress-live.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "jg2uuq",
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
